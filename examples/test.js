bdf0469832c3cfee03dc63760a2efbf659c01adf
Bucky24 <rwijtman@gmail.com>
2022-09-27 22:00:37 -0700

    absolute positioning

---
+++ examples/test.js
@@ -0,0 +1,10 @@
+var list = [
+    document.createElement("div"),
+    document.createElement("div"),
+    document.createElement("div"),
+];
+
+for (let i=0;i<list.length;i++) {
+    var thing = list[i];
+    console.log(thing);
+}
