d0054f16bcdc1fccdd3cf3b376313ba96470346a
Bucky24 <rwijtman@gmail.com>
2022-10-04 17:58:13 -0700

    parse int example file

---
+++ examples/parse_int_example.js
@@ -0,0 +1,10 @@
+var numberAsString = "12.534";
+console.log(numberAsString, typeof numberAsString);
+var numberFromString = parseInt(numberAsString);
+console.log(numberFromString, typeof numberFromString);
+var floatFromString = parseFloat(numberAsString);
+console.log(floatFromString, typeof floatFromString);
+
+var str = 'abcd';
+var str2 = str.substring(0, 3);
+console.log(str[0], str2);
