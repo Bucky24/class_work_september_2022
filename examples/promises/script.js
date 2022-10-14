598d84990b0992e2260418334c3d6ed608ccf398
Bucky24 <rwijtman@gmail.com>
2022-10-13 17:35:20 -0700

    preload comments

--- examples/promises/script.js
+++ examples/promises/script.js
@@ -1,1 +1,5 @@
-// your code here
+// use webRequest to call apis, it takes the api, then a callback that provides the result
+// our apis are:
+// best_singer
+// albums/{{ singer name }}
+// song_list/{{ album name }}
