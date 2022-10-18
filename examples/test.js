1bbe173f03e893a4c5f674e20613a12c4374cf82
Bucky24 <rwijtman@gmail.com>
2022-10-17 20:52:44 -0700

    commit test

---
+++ examples/test.js
@@ -0,0 +1,9 @@
+// Run the following code and explain the output:
+
+var sample = function() {
+    b = 3;
+    var a = 3;
+}
+sample();
+console.log("Is a defined?", typeof a !== "undefined");
+console.log("Is b defined?", typeof b !== "undefined");
