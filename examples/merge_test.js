c9fa38f021b1489e68c4c7af706b670f9180908f
Bucky24 <rwijtman@gmail.com>
2022-10-25 11:11:08 -0700

    add in subtract number

--- examples/merge_test.js
+++ examples/merge_test.js
@@ -2,8 +2,13 @@ function getNumber(a, b) {
     return a + b;
 }
 
+function subtractNumber(a, b) {
+    return a - b;
+}
+
 function doOperation(a, b, fn) {
     return fn(a, b);
 }
 
 var result = doOperation(5, 10, getNumber);
+var result2 = doOperation(5, 10, subtractNumber);
